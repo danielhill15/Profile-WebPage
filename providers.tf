3dff59eeb7ce184960975cc0e8a2718e4bf4e7e0
danielhill15 <54922228+danielhill15@users.noreply.github.com>
2023-03-26 23:07:52 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,12 @@
 terraform {
   required_providers {
+    cloud {
+      organization = "danielhill15"
+
+      workspaces {
+        name = "example-workspace"
+    }
+ }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.26.0"
