a30e7febdfda344db5686751abaa48b694bdbadd
danielhill15 <54922228+danielhill15@users.noreply.github.com>
2023-03-28 19:43:11 -0400

    Add lambda to tf

--- main.tf
+++ main.tf
@@ -116,3 +116,63 @@ output "route53_domain" {
 output "cdn_domain" {
   value = "${aws_cloudfront_distribution.cdn.domain_name}"
 }
+
+resource "aws_iam_role" "lambda_role" {
+name   = "Lambda_Function_Role"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+ 
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path         = "/"
+ description  = "AWS IAM Policy for managing aws lambda role"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "dynamodb:*"
+     ],
+     "Resource": "*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+data "archive_file" "zip_the_python_code" {
+type        = "zip"
+source_dir  = "${path.module}/lambda/"
+output_path = "${path.module}/lambda/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+filename                       = "${path.module}/lambda/lambda_function.zip"
+function_name                  = "Lambda_Function"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "python3.9"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
