7b6d4da3e2c40e504fcc6518ea9a031c73d7a8b5
danielhill15 <54922228+danielhill15@users.noreply.github.com>
2023-04-18 23:31:16 -0400

    Update get_random_quotes.py

--- lambda/get_random_quotes.py
+++ lambda/get_random_quotes.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     conn = http.client.HTTPSConnection("andruxnet-random-famous-quotes.p.rapidapi.com")
 
     headers = {
-        'X-RapidAPI-Key': "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        'X-RapidAPI-Key': "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
         'X-RapidAPI-Host': "andruxnet-random-famous-quotes.p.rapidapi.com"
         }
     
@@ -21,10 +21,19 @@ def lambda_handler(event, context):
     
     file_name = "quotes.txt"
     lambda_path = "/tmp/" + file_name
-    s3_path = "/100001/20180223/" + file_name
     
-    with open(lambda_path, 'wb') as file:
-        file.write(data)
+    with open(lambda_path, 'w') as file:
+        file.write("      Random Quotes of the Day")
+        file.write('\n')
+        file.write('\n')
+        for quote in json.loads(data.decode("utf-8"))[:5]:
+            q = quote['quote']
+            a = '      - {}'.format(quote['author'])
+            file.write(q)
+            file.write('\n')
+            file.write(a)
+            file.write('\n')
+            file.write('\n')
         file.close()
         
     s3 = boto3.resource('s3')
