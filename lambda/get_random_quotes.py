9b9ad0712e5cf520d8fa89793a82d967352cc83b
danielhill15 <54922228+danielhill15@users.noreply.github.com>
2023-04-18 23:08:12 -0400

    Update get_random_quotes.py

--- lambda/get_random_quotes.py
+++ lambda/get_random_quotes.py
@@ -1,21 +1,39 @@
 import json
 import http.client
+import boto3
+
+s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
 
     conn = http.client.HTTPSConnection("andruxnet-random-famous-quotes.p.rapidapi.com")
 
     headers = {
-        'X-RapidAPI-Key': "5677e70ba2mshfbef6c9f5f96818p19744fjsn25c4b7008942",
+        'X-RapidAPI-Key': "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
         'X-RapidAPI-Host': "andruxnet-random-famous-quotes.p.rapidapi.com"
         }
     
     conn.request("GET", "/?cat=famous&count=10", headers=headers)
     
     res = conn.getresponse()
+    print(res)
     data = res.read()
     
+    file_name = "quotes.txt"
+    lambda_path = "/tmp/" + file_name
+    s3_path = "/100001/20180223/" + file_name
+    
+    with open(lambda_path, 'wb') as file:
+        file.write(data)
+        file.close()
+        
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file(lambda_path, 'daniel-barak-hill.io', file_name, 
+    ExtraArgs={'ContentDisposition' : 'inline','ACL': 'public-read','ContentType':'text/plain'}
+    )
+    
     print(data.decode("utf-8"))
+
     # TODO implement
     return {
         'statusCode': 200,
